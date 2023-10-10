8d571c1f46095fb7205b7d38e9ae774f01cbe607
Monica <mmunzp@gmail.com>
2023-10-10 17:18:08 +0200

    Exercici 6

---
+++ S2.1 - Exercicis bàsics/Exercici 1.4 - Rest & Spread operators/Nivell 3/Exercici 6/script.js
@@ -0,0 +1,13 @@
+ const objecte1 = {
+    nom : "antonia",
+    edat : "100",
+}
+
+ const objecte2 = {
+    lloc : "barcelona",
+    any : "3099",
+}
+
+const objecte3 = {...objecte1, ...objecte2};
+console.log(objecte3);
+

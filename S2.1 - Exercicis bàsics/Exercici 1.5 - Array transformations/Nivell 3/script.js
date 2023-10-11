ef44ce0a2e71b7b70aed4493236438c54fa67918
Monica <mmunzp@gmail.com>
2023-10-11 21:01:35 +0200

    Nivell 3 arrays

---
+++ S2.1 - Exercicis bàsics/Exercici 1.5 - Array transformations/Nivell 3/script.js
@@ -0,0 +1,16 @@
+const array = [11, 12, 13, 14];
+const tots = array.every(num => num > 10);
+
+if (tots) {
+  console.log("Tots son majors de 10");
+} else {
+  console.log("Almenys un és major que 10");
+}
+
+const alguns = array.some(num => num > 10);
+
+if (alguns) {
+  console.log("Almenys un és major que 10");
+} else {
+  console.log("Cap és major que 10");
+}

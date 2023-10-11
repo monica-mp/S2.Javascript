ae1da8290bad39ce8fa794a0d506475e03f01510
Monica <mmunzp@gmail.com>
2023-10-11 19:22:40 +0200

    Exercici 4

---
+++ S2.1 - Exercicis bàsics/Exercici 1.7 - Promises & Async_Await/Nivell 1/Exercici 4/script.js
@@ -0,0 +1,18 @@
+function promesa(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve('Hola, món');
+        }, 2000); 
+    });
+}
+
+async function funcioAsincrona(){
+  try{
+    const result = await promesa();
+    console.log(result);
+  } catch(error){
+      console.error(error);
+  }
+}
+
+funcioAsincrona();
